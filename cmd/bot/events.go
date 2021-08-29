174bebe78928acd5af161409d2321037d0464aa6
zneix <zneix@zneix.eu>
2021-08-29 15:37:59 +0200

    So this is what the check was all about

--- cmd/bot/events.go
+++ cmd/bot/events.go
@@ -74,7 +74,6 @@ func registerEvents(tcb *bot.Bot) {
 
 		userType, ok := message.Tags["user-type"]
 		switch {
-
 		case !ok:
 			log.Println("[USERSTATE] user-type tag was not found in the IRC message, either no capabilities or Twitch removed this tag xd")
 
