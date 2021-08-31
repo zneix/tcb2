482a9d4b0ead9e0cac2653f91a8ef27cb8f00294
zneix <zneix@zneix.eu>
2021-08-31 17:51:48 +0200

    Disable logging on message queue start

--- internal/bot/channel.go
+++ internal/bot/channel.go
@@ -12,8 +12,8 @@ import (
 )
 
 func (channel *Channel) StartMessageQueue(twitchIRC *twitch.Client) {
-	log.Println("Starting message queue for " + channel.String())
-	defer log.Println("Done with message queue for " + channel.String())
+	// log.Println("Starting message queue for " + channel.String())
+	defer log.Println("[Channel] Message queue suddenly quit(?) for " + channel.String())
 
 	for message := range channel.QueueChannel {
 		// Actually send the message to the chat
