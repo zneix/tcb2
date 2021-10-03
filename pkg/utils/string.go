0b99cc0bdb8e76ea8cf9deca782a8ead90e65a9e
41973452 <41973452+Felanbird@users.noreply.github.com>
2021-10-02 21:16:10 -0400

    Fix typo & correct captilization in a couple comments

--- pkg/utils/string.go
+++ pkg/utils/string.go
@@ -2,7 +2,7 @@ package utils
 
 import "unicode/utf8"
 
-// LimitString makes sure that a is not bigger than limit or as long as limit with the appended unicode that looks like three dots
+// LimitString makes sure that a is not bigger than limit or as long as limit with the appended Unicode that looks like three dots
 func LimitString(a string, limit int) string {
 	if utf8.RuneCountInString(a) > limit {
 		return a[0:limit-1] + "…"
