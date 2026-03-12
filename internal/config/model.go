8205adfd24aca3a42efa42319175c589792e46b6
zneix <zneix@zneix.eu>
2026-03-12 18:28:29 +0100

    Parse config file directly without viper

--- internal/config/model.go
+++ internal/config/model.go
@@ -1,32 +1,52 @@
 package config
 
 type TCBConfig struct {
-	// Bot
+	/// Bot
 
-	CommandPrefix string `mapstructure:"command-prefix"`
+	// CommandPrefix prefix to which bot will respond to (chat commands)
+	CommandPrefix string `yaml:"command-prefix"`
 
-	// Misc
+	/// Misc
 
-	SupinicAPIKey string `mapstructure:"supinic-api-key"`
+	// SupinicAPIKey bot's key to the Supinic's API, in the format: "SupibotID:APIKey" without quotation marks
+	SupinicAPIKey string `yaml:"supinic-api-key"`
 
-	// API
+	/// API
 
-	BaseURL     string `mapstructure:"base-url"`
-	BindAddress string `mapstructure:"bind-address"`
+	// BaseURL url of the API to which clients will make their requests. Useful if the API is proxied through reverse proxy like nginx.
+	// Its value needs to contain full URL with protocol scheme, e.g. https://braize.pajlada.com/chatterino
+	BaseURL string `yaml:"base-url"`
+	// BindAddress address to which API will bind and start listening on
+	BindAddress string `yaml:"bind-address"`
 
-	// Twitch
+	/// Twitch
 
-	TwitchLogin          string `mapstructure:"twitch-login"`
-	TwitchOAuth          string `mapstructure:"twitch-oauth"`
-	TwitchClientID       string `mapstructure:"twitch-client-id"`
-	TwitchClientSecret   string `mapstructure:"twitch-client-secret"`
-	TwitchEventSubSecret string `mapstructure:"twitch-eventsub-secret"`
+	// TwitchLogin login name of the account on which bot will log into Twitch's IRC on the Write connection
+	TwitchLogin string `yaml:"twitch-login"`
+	// TwitchOAuth OAuth token of the account on which bot will log into Twitch's IRC on the Write connection
+	// It should not have the "oauth:" prefix - that is added once bot attempts to authenticate
+	TwitchOAuth string `yaml:"twitch-oauth"`
+	// TwitchClientID
+	TwitchClientID string `yaml:"twitch-client-id"`
+	// TwitchClientSecret
+	TwitchClientSecret string `yaml:"twitch-client-secret"`
+	// TwitchEventSubSecret secret used to create subscriptions and verify incoming notifications
+	// Must be between 10 and 100 characters long
+	TwitchEventSubSecret string `yaml:"twitch-eventsub-secret"`
 
-	// Mongo 🥭
+	/// Mongo 🥭
 
-	MongoUsername     string `mapstructure:"mongo-username"`
-	MongoPassword     string `mapstructure:"mongo-password"`
-	MongoPort         string `mapstructure:"mongo-port"`
-	MongoDatabaseName string `mapstructure:"mongo-database-name"`
-	MongoAuthDB       string `mapstructure:"mongo-auth-db"`
+	// MongoUsername
+	MongoUsername string `yaml:"mongo-username"`
+	// MongoPassword
+	MongoPassword string `yaml:"mongo-password"`
+	// MongoPort port to which connection will try to connect
+	// Host is hardcoded to localhost due to security concerns (use ssh port tunneling while testing/developing on a remote machine)
+	MongoPort string `yaml:"mongo-port"`
+	// MongoDatabaseNamename name of the database used by the bot
+	// It's treated as a prefix to allow using separate databases while testing/developing
+	MongoDatabaseName string `yaml:"mongo-database-name"`
+	// MongoAuthDB name of authentication databse, used as AuthSource while creating a new mongo.Connection
+	// This should usually be left unchanged
+	MongoAuthDB string `yaml:"mongo-auth-db"`
 }
